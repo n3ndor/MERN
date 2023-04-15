dd50bda2f027f6a3a73fd8da853910ba80036beb
n3ndor <n3ndor@gmail.com>
2023-04-14 23:20:03 -0400

    added more routing, read the rules at the landing page

--- routing-practice/src/App.js
+++ routing-practice/src/App.js
@@ -1,14 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 
 const Root = (props) => {
   return (
     <div>
-      <h2 style={{ background: "red", padding: "50px" }}>Welcome to Routing Practice</h2>
+      <h2 style={{ background: "#000080", color: "white", padding: "50px" }}>Welcome to Routing Practice</h2>
       <div style={{ marginLeft: "50px" }}>
-        <h2>There are different ways what you can test out: </h2>
-        <h2>To get started, change the AddressBar input to localhost:3000/...</h2>
+        <h2>There are different ways what you can test out! </h2>
+        <h2>To get started, change the AddressBar to:</h2>
         <h3>localhost:3000/home</h3>
         <h3>localhost:3000/about</h3>
+        <h3>localhost:3000/ (add a text or number [<span style={{ color: "red" }}>caution!</span>  in this case text+number=text ])</h3>
+        <h2 style={{ background: "#000080", color: "white", padding: "20px", width: "300px" }}>Fun begins with colors!</h2>
+        <h3>localhost:3000/text/textcolor</h3>
+        <h3>localhost:3000/text/textcolor/backroundcolor</h3>
       </div>
     </div>
   );
@@ -26,6 +30,19 @@ const About = (props) => {
   );
 }
 
+const Text = (props) => {
+  const { text, color, bgCol } = useParams();
+  return (
+    <div>
+      {
+        isNaN(text)
+          ? <h2 style={color ? { color: color, backgroundColor: bgCol, padding: "50px" } : { padding: "50px" }}>Your input is a text, look: {text}</h2>
+          : <h2 style={{ padding: "50px" }}>Your input is a number, look: {text} </h2>
+      }
+    </div>
+  )
+}
+
 
 
 function App() {
@@ -37,6 +54,9 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/home" element={<Home />} />
           <Route path="/" element={<Root />} />
+          <Route path="/:text" element={<Text />} />
+          <Route path="/:text/:color" element={<Text />} />
+          <Route path="/:text/:color/:bgCol" element={<Text />} />
         </Routes>
 
       </BrowserRouter>
